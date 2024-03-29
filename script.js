3bcac1d662bf85e43f33a62074827d56438c5e9d
Rafael T <rt.lucena@outlook.com>
2024-03-29 05:37:53 -0300

    Adicionando um botao em JavaScript para verificar se o usuario e maior de 18 anos

---
+++ script.js
@@ -0,0 +1,19 @@
+const botaoMaior = document.getElementById('maiorIdade');
+const botaoMenor = document.getElementById('menorIdade');
+const conteudoSite = document.getElementById('main');
+
+botaoMaior.addEventListener('click', () => {
+    botaoMaior.style.display = 'none';
+    botaoMenor.style.display = 'none';
+    conteudoSite.style.display = 'block';
+});
+
+botaoMenor.addEventListener('click', () => {
+    const idadeUsuario = 17;
+    if (idadeUsuario < 18) {
+        window.location.href = 'https://www.google.com';
+    } else {
+        botaoMenor.style.display = 'none';
+        conteudoSite.style.display = 'block';
+    }
+});
